e854df66b96130ccffccb33041d633180f3f3341
stefanscript <sha32768@gmail.com>
2021-02-13 18:31:53 +0000

    customize tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: {
+        content: [
+            "./public/index.html"
+        ], options: {
+            keyframes: true,
+        },
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.trueGray,
+            indigo: colors.indigo,
+            red: colors.rose,
+            blue: colors.blue,
+            yellow: colors.amber,
+        }
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+    corePlugins: {
+        float: false
+    }
+};
